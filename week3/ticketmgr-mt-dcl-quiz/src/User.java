484cd704a0862c879e29bedf23c46e06da38ef5a
insang <golsung@naver.com>
2024-08-09 13:53:25 +0900

    code for week 3

---
+++ week3/ticketmgr-mt-dcl-quiz/src/User.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+public class User extends Thread{
+    public User(String name) {
+        super(name);
+    }
+    public void run() {
+        TicketManager mgr = TicketManager.getInstance();
+        Optional<Ticket> t = mgr.issue();
+        t.ifPresent(s->System.out.println(Thread.currentThread().getName() + " " + mgr +
+                " end watching Movie with " + s.getSerialNum()));
+    }
+}
